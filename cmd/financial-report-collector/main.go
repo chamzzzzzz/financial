f8a6f5f9848676d4ca76ca782d040a724204f2fc
Liu Chen <chamzzzzzz@gmail.com>
2023-04-22 23:37:10 +0800

    Init commit

---
+++ cmd/financial-report-collector/main.go
@@ -0,0 +1,312 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/chamzzzzzz/financial/source/cninfo"
+	"github.com/urfave/cli/v2"
+)
+
+type AnnualReport struct {
+	Year        int
+	Title       string
+	URL         string
+	PublishTime int64
+}
+
+type Stock struct {
+	Code                  string
+	Name                  string
+	Pinyin                string
+	OrgID                 string
+	AnnualReportCheckTime int64
+	AnnualReports         []*AnnualReport
+}
+
+type Database struct {
+	Stocks  []*Stock
+	indexes map[string]*Stock `json:"-"`
+}
+
+type Option struct {
+	SinceYear2000              bool
+	LatestThreeYears           bool
+	SpecifiedYears             []int
+	AnnualReportCheckInterval  int64
+	AnnualReportUpdateInterval int64
+	File                       string
+}
+
+type App struct {
+	option   Option
+	database Database
+	start    time.Time
+	end      time.Time
+	years    []int
+}
+
+func (app *App) Run() error {
+	c := &cli.App{
+		Name:  "financial-report-collector",
+		Usage: "financial-report-collector",
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "since-year-2000",
+				Usage:       "since year 2000",
+				Destination: &app.option.SinceYear2000,
+				Value:       false,
+			},
+			&cli.BoolFlag{
+				Name:        "latest-three-years",
+				Usage:       "latest three years",
+				Destination: &app.option.LatestThreeYears,
+				Value:       true,
+			},
+			&cli.MultiIntFlag{
+				Target: &cli.IntSliceFlag{
+					Name:  "specified-years",
+					Usage: "specified years",
+				},
+				Value:       []int{},
+				Destination: &app.option.SpecifiedYears,
+			},
+			&cli.Int64Flag{
+				Name:        "annual-report-check-interval",
+				Usage:       "annual report check interval",
+				Destination: &app.option.AnnualReportCheckInterval,
+				Value:       24 * 3600,
+			},
+			&cli.Int64Flag{
+				Name:        "annual-report-update-interval",
+				Usage:       "annual report update interval",
+				Destination: &app.option.AnnualReportUpdateInterval,
+				Value:       100,
+			},
+			&cli.StringFlag{
+				Name:        "file",
+				Usage:       "database file",
+				Destination: &app.option.File,
+				Value:       "annualreport.db",
+			},
+		},
+		Action: app.action,
+	}
+	return c.Run(os.Args)
+}
+
+func (app *App) loadDatabase() error {
+	if _, err := os.Stat(app.option.File); err == nil {
+		b, err := os.ReadFile(app.option.File)
+		if err != nil {
+			return err
+		}
+		err = json.Unmarshal(b, &app.database)
+		if err != nil {
+			return err
+		}
+	}
+	app.database.indexes = make(map[string]*Stock)
+	for _, stock := range app.database.Stocks {
+		app.database.indexes[stock.Code] = stock
+	}
+	return nil
+}
+
+func (app *App) saveDatabase() error {
+	b, err := json.MarshalIndent(&app.database, "", "  ")
+	if err != nil {
+		return err
+	}
+	os.WriteFile(app.option.File, b, 0644)
+	return nil
+}
+
+func (app *App) updateStock() error {
+	source := &cninfo.Source{}
+	stocks, err := source.GetStockList()
+	if err != nil {
+		return err
+	}
+	for _, v := range stocks {
+		if stock := app.database.indexes[v.Code]; stock == nil {
+			stock = &Stock{
+				Code:   v.Code,
+				Name:   v.Zwjc,
+				Pinyin: v.Pinyin,
+				OrgID:  v.OrgID,
+			}
+			app.database.Stocks = append(app.database.Stocks, stock)
+			app.database.indexes[stock.Code] = stock
+			fmt.Printf("add stock %+v\n", stock)
+		}
+	}
+	sort.Slice(app.database.Stocks, func(i, j int) bool { return app.database.Stocks[i].Code < app.database.Stocks[j].Code })
+	return nil
+}
+
+func (app *App) updateAnnualReport() error {
+	now := time.Now()
+	var stocks []*Stock
+	for _, stock := range app.database.Stocks {
+		if now.Unix()-stock.AnnualReportCheckTime < app.option.AnnualReportCheckInterval {
+			fmt.Printf("skip stock %s annual report\n", stock.Code)
+			continue
+		}
+		stocks = append(stocks, stock)
+	}
+
+	if len(app.option.SpecifiedYears) > 0 {
+		min, max := app.option.SpecifiedYears[0], app.option.SpecifiedYears[0]
+		for _, year := range app.option.SpecifiedYears {
+			if year < min {
+				min = year
+			}
+			if year > max {
+				max = year
+			}
+		}
+		app.start = time.Date(min, 1, 1, 0, 0, 0, 0, time.Local)
+		app.end = time.Date(max, 12, 31, 0, 0, 0, 0, time.Local)
+		if app.start.Before(time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local)) {
+			app.start = time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local)
+		}
+		if app.end.After(now) {
+			app.end = now
+		}
+	} else {
+		if app.option.SinceYear2000 {
+			app.start = time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local)
+			app.end = now
+		} else if app.option.LatestThreeYears {
+			app.start = now.AddDate(-3, 0, 0)
+			app.end = now
+		}
+		t := app.start
+		for {
+			if t.After(app.end) {
+				break
+			}
+			app.years = append(app.years, t.Year())
+			t = t.AddDate(1, 0, 0)
+		}
+	}
+
+	i := 0
+	for _, stock := range stocks {
+		reports, err := app.getAnnualReports(stock)
+		if err != nil {
+			return err
+		}
+		for _, report := range reports {
+			for _, v := range stock.AnnualReports {
+				if *v == *report {
+					fmt.Printf("skip stock %s dup annual report %+v\n", stock.Code, report)
+					continue
+				}
+			}
+			stock.AnnualReports = append(stock.AnnualReports, report)
+			fmt.Printf("add stock %s annual report %+v\n", stock.Code, report)
+		}
+		stock.AnnualReportCheckTime = time.Now().Unix()
+
+		i++
+		fmt.Printf("update stock annual report %d/%d\n", i, len(stocks))
+		if i%10 == 0 {
+			fmt.Printf("sleep %d milliseconds\n", app.option.AnnualReportUpdateInterval*2)
+			time.Sleep(time.Duration(app.option.AnnualReportUpdateInterval * 2 * int64(time.Millisecond)))
+			if err := app.saveDatabase(); err != nil {
+				return err
+			}
+		} else {
+			fmt.Printf("sleep %d milliseconds\n", app.option.AnnualReportUpdateInterval)
+			time.Sleep(time.Duration(app.option.AnnualReportUpdateInterval * int64(time.Millisecond)))
+		}
+	}
+	if err := app.saveDatabase(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (app *App) getAnnualReports(stock *Stock) ([]*AnnualReport, error) {
+	source := &cninfo.Source{}
+	announcements, err := source.GetAnnualReportAnnoucements(&cninfo.Stock{Code: stock.Code, OrgID: stock.OrgID}, app.start, app.end)
+	if err != nil {
+		for i := 0; i < 3; i++ {
+			fmt.Printf("get stock %s annual report error: %s\n", stock.Code, err)
+			fmt.Printf("retry after 5 seconds\n")
+			time.Sleep(time.Second * 5)
+			announcements, err = source.GetAnnualReportAnnoucements(&cninfo.Stock{Code: stock.Code, OrgID: stock.OrgID}, app.start, app.end)
+			if err == nil {
+				break
+			}
+			if i == 2 {
+				return nil, err
+			}
+		}
+	}
+	var reports []*AnnualReport
+	for _, announcement := range announcements {
+		if strings.ToUpper(announcement.AdjunctType) != "PDF" {
+			continue
+		}
+
+		year := time.Unix(announcement.AnnouncementTime/1000, 0).Year() - 1
+		matched := false
+		for _, y := range app.years {
+			if y == year {
+				matched = true
+				break
+			}
+		}
+		if !matched {
+			continue
+		}
+
+		report := &AnnualReport{
+			Year:        year,
+			Title:       announcement.AnnouncementTitle,
+			URL:         "http://static.cninfo.com.cn/" + announcement.AdjunctURL,
+			PublishTime: announcement.AnnouncementTime,
+		}
+		reports = append(reports, report)
+	}
+	return reports, nil
+}
+
+func (app *App) action(c *cli.Context) error {
+	err := app.loadDatabase()
+	if err != nil {
+		return err
+	}
+
+	err = app.updateStock()
+	if err != nil {
+		return err
+	}
+
+	err = app.saveDatabase()
+	if err != nil {
+		return err
+	}
+
+	err = app.updateAnnualReport()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	app := &App{}
+	err := app.Run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ source/cninfo/cninfo.go
@@ -0,0 +1,192 @@
+package cninfo
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Stock struct {
+	Code     string `json:"code"`
+	Pinyin   string `json:"pinyin"`
+	Category string `json:"category"`
+	OrgID    string `json:"orgId"`
+	Zwjc     string `json:"zwjc"`
+}
+
+type StockListResponse struct {
+	StockList []*Stock `json:"stockList"`
+}
+
+type Announcement struct {
+	SecCode           string `json:"secCode"`
+	SecName           string `json:"secName"`
+	OrgID             string `json:"orgId"`
+	AnnouncementID    string `json:"announcementId"`
+	AnnouncementTitle string `json:"announcementTitle"`
+	AnnouncementTime  int64  `json:"announcementTime"`
+	AdjunctURL        string `json:"adjunctUrl"`
+	AdjunctSize       int    `json:"adjunctSize"`
+	AdjunctType       string `json:"adjunctType"`
+	TileSecName       string `json:"tileSecName"`
+	ShortTitle        string `json:"shortTitle"`
+}
+
+type HisAnnouncementQueryRequest struct {
+	PageNum   int    `json:"pageNum"`
+	PageSize  int    `json:"pageSize"`
+	Column    string `json:"column"`
+	TabName   string `json:"tabName"`
+	Plate     string `json:"plate"`
+	Stock     string `json:"stock"`
+	Searchkey string `json:"searchkey"`
+	Secid     string `json:"secid"`
+	Category  string `json:"category"`
+	Trade     string `json:"trade"`
+	SeDate    string `json:"seDate"`
+	SortName  string `json:"sortName"`
+	SortType  string `json:"sortType"`
+	IsHLtitle string `json:"isHLtitle"`
+}
+
+type HisAnnouncementQueryResponse struct {
+	TotalSecurities   int             `json:"totalSecurities"`
+	TotalAnnouncement int             `json:"totalAnnouncement"`
+	TotalRecordNum    int             `json:"totalRecordNum"`
+	Announcements     []*Announcement `json:"announcements"`
+	HasMore           bool            `json:"hasMore"`
+	Totalpages        int             `json:"totalpages"`
+}
+
+func (q *HisAnnouncementQueryRequest) FormURLEncoded() string {
+	v := *q
+	if v.PageNum == 0 {
+		v.PageNum = 1
+	}
+	if v.PageSize == 0 {
+		v.PageSize = 30
+	}
+	if v.Column == "" {
+		v.Column = "szse"
+	}
+	if v.TabName == "" {
+		v.TabName = "fulltext"
+	}
+	if v.Stock == "" {
+		v.Stock = "000001,gssz0000001"
+	}
+	if v.Category == "" {
+		v.Category = "category_ndbg_szsh"
+	}
+	if v.IsHLtitle == "" {
+		v.IsHLtitle = "true"
+	}
+	if v.SeDate == "" {
+		now := time.Now()
+		v.SeDate = fmt.Sprintf("%s~%s", now.AddDate(-1, 0, 0).Format("2006-01-02"), now.AddDate(0, 0, 1).Format("2006-01-02"))
+	}
+	return fmt.Sprintf("pageNum=%d&pageSize=%d&column=%s&tabName=%s&plate=%s&stock=%s&searchkey=%s&secid=%s&category=%s&trade=%s&seDate=%s&sortName=%s&sortType=%s&isHLtitle=%s", v.PageNum, v.PageSize,
+		url.QueryEscape(v.Column), url.QueryEscape(v.TabName), url.QueryEscape(v.Plate), url.QueryEscape(v.Stock), url.QueryEscape(v.Searchkey), url.QueryEscape(v.Secid), url.QueryEscape(v.Category),
+		url.QueryEscape(v.Trade), v.SeDate, url.QueryEscape(v.SortName), url.QueryEscape(v.SortType), url.QueryEscape(v.IsHLtitle))
+}
+
+type Source struct {
+}
+
+func (s *Source) RequestStockList() (*StockListResponse, error) {
+	resp, err := http.Get("http://www.cninfo.com.cn/new/data/szse_stock.json")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	p := &StockListResponse{}
+	if err := json.NewDecoder(resp.Body).Decode(p); err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func (s *Source) GetStockList() ([]*Stock, error) {
+	p, err := s.RequestStockList()
+	if err != nil {
+		return nil, err
+	}
+	return p.StockList, nil
+}
+
+func (s *Source) RequestHisAnnouncementQuery(q *HisAnnouncementQueryRequest) (*HisAnnouncementQueryResponse, error) {
+	client := &http.Client{}
+	form := q.FormURLEncoded()
+	fmt.Println(form)
+	req, err := http.NewRequest("POST", "http://www.cninfo.com.cn/new/hisAnnouncement/query", bytes.NewBufferString(form))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Accept-Language", "zh-CN,zh-Hans;q=0.9")
+	req.Header.Set("Host", "www.cninfo.com.cn")
+	req.Header.Set("Origin", "http://www.cninfo.com.cn")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Referer", "http://www.cninfo.com.cn/new/commonUrl/pageOfSearch?url=disclosure/list/search&lastPage=index")
+	req.Header.Set("Content-Length", strconv.Itoa(len(form)))
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	p := &HisAnnouncementQueryResponse{}
+	if err := json.NewDecoder(resp.Body).Decode(p); err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func (s *Source) GetAnnualReportAnnoucements(stock *Stock, start, end time.Time) ([]*Announcement, error) {
+	announcements := []*Announcement{}
+	if start.After(end) {
+		return nil, errors.New("start time must be before end time")
+	}
+	if end.Sub(start).Hours() > 24*365*30 {
+		return nil, errors.New("time range must be less than 30 years")
+	}
+
+	from := start
+	to := from.AddDate(3, 0, 0)
+	if to.After(end) {
+		to = end
+	}
+	for {
+		p, err := s.RequestHisAnnouncementQuery(&HisAnnouncementQueryRequest{
+			Stock:    strings.Join([]string{stock.Code, stock.OrgID}, ","),
+			Category: "category_ndbg_szsh",
+			SeDate:   fmt.Sprintf("%s~%s", from.Format("2006-01-02"), to.Format("2006-01-02")),
+		})
+		if err != nil {
+			return nil, err
+		}
+		c := len(p.Announcements)
+		if c > 0 {
+			for i := c - 1; i >= 0; i-- {
+				announcements = append(announcements, p.Announcements[i])
+			}
+		}
+		if to.Equal(end) || to.After(end) {
+			break
+		}
+		from = to.AddDate(0, 0, 1)
+		to = from.AddDate(3, 0, 0)
+		if to.After(end) {
+			to = end
+		}
+	}
+	return announcements, nil
+}
